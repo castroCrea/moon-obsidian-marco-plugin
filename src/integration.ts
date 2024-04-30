e1e0d6647e6f2bfd128c56a0f57501e1e193d367
Castro Crea <castro.crea@gmail.com>
2024-04-30 12:12:06 +0200

    ✨ Get Path

--- src/integration.ts
+++ src/integration.ts
@@ -3,34 +3,8 @@ import { type Context } from '@moonjot/moon'
 import { extractTitleFromMarkdown } from './extractTitleFromMarkdown'
 import fs from 'fs'
 import path from 'path'
-
-/**
- * TODO
- * HANDLE IF
- * get PATH
- */
-
-type StartEndAnchor = {
-  startAnchor: '${START_NOTE}'
-  endAnchor: '${END_NOTE}'
-} | {
-  startAnchor: '${START_PERSON}'
-  endAnchor: '${END_PERSON}'
-} | {
-  startAnchor: '${START_JOURNAL}'
-  endAnchor: '${END_JOURNAL}'
-} | {
-  startAnchor: '${START_SOURCE}'
-  endAnchor: '${END_SOURCE}'
-}
-
-const extractTextFromAnchor = ({
-  text,
-  startAnchor,
-  endAnchor
-}: { text: string } & StartEndAnchor): string | undefined => {
-  return (text.split(endAnchor)[0].split(startAnchor)[1])?.trim() // faster than the regex x10
-}
+import { type SearchObject, type LOG } from './types'
+import { extractTextFromAnchor } from './extractText'
 
 type Indexable = Record<string, any>
 
@@ -51,7 +25,7 @@ const searchObject = ({
   return current // Return the value found at the end of the path
 }
 
-const turnKeyIntoContent = ({ content, searchObj }: { content?: string, searchObj: object }) => {
+const turnKeyIntoContent = ({ content, searchObj }: { content?: string, searchObj: SearchObject }) => {
   const regex = /\$\{((.)*?)\}/gm
 
   const matches = content?.match(regex)
@@ -75,11 +49,57 @@ const turnKeyIntoContent = ({ content, searchObj }: { content?: string, searchOb
   return content
 }
 
-export const doIntegration = ({ markdown, pathToTemplates, log, context }: { markdown: string, pathToTemplates?: string, log: ((log: string) => void) | undefined, context: Context }) => {
+const handleConditions = ({ content, searchObj }: { content?: string, searchObj: SearchObject }) => {
+  const regexIf = /\$\{IF.*?\}(.*)\$\{END_IF.*?\}/gm
+  const regexIfStart = /\$\{IF ((.)*?)\}/gm
+  const regexIfEnd = /\$\{END_IF ((.)*?)\}/gm
+  const matches = content?.match(regexIf)
+  console.log('ifArray', { matches })
+
+  matches?.forEach(value => {
+    const ifValue = value.match(regexIfStart)?.[0]
+    if (!ifValue) return
+    const key = (ifValue.replace('${IF ', '').replace('}', '')).toLowerCase()
+
+    const keyValue = searchObject({ obj: searchObj, path: key })
+    console.log({ ifValue, key, keyValue })
+
+    if (!keyValue) {
+      content = content?.replace(value, '')
+    } else {
+      let valueReplaced = value
+      const endIfValue = value.match(regexIfEnd)?.[0]
+      if (ifValue) valueReplaced = valueReplaced?.replace(ifValue, '')
+      if (endIfValue) valueReplaced = valueReplaced?.replace(endIfValue, '')
+      valueReplaced = turnKeyIntoContent({ content: valueReplaced, searchObj }) ?? ''
+      content = content?.replace(value, valueReplaced)
+    }
+  })
+
+  return content
+}
+
+const getPath = ({ noteContent, log, searchObj }: { noteContent: string, log: LOG, searchObj: SearchObject }) => {
+  const pathContent = (noteContent.split('${END_PATH}')[0].split('${PATH}')[1])?.trim()
+
+  const lines = pathContent.split('\n')
+
+  let notePath: string | undefined
+
+  for (const line of lines) {
+    notePath = handleConditions({ content: line ?? '', searchObj })
+    if (notePath) break
+  }
+
+  return undefined
+}
+
+export const doIntegration = ({ markdown, pathToTemplates, log, context }: { markdown: string, pathToTemplates?: string, log: LOG, context: Context }) => {
   if (!pathToTemplates) return false
   const defaultTemplate = fs.readFileSync(path.join(pathToTemplates, 'default.md'), 'utf8')
 
   const noteTemplate = extractTextFromAnchor({ text: defaultTemplate, startAnchor: '${START_NOTE}', endAnchor: '${END_NOTE}' })
+
   const peopleTemplate = extractTextFromAnchor({ text: defaultTemplate, startAnchor: '${START_PERSON}', endAnchor: '${END_PERSON}' })
   const sourceTemplate = extractTextFromAnchor({ text: defaultTemplate, startAnchor: '${START_SOURCE}', endAnchor: '${END_SOURCE}' })
   const journalTemplate = extractTextFromAnchor({ text: defaultTemplate, startAnchor: '${START_JOURNAL}', endAnchor: '${END_JOURNAL}' })
@@ -87,15 +107,12 @@ export const doIntegration = ({ markdown, pathToTemplates, log, context }: { mar
   const title = extractTitleFromMarkdown(markdown)
   const content = markdown
 
-  const searchObj = {
+  const searchObj: SearchObject = {
     ...context,
     title,
     content
   }
-
-  const regexIf = /\$\{IF.*?\}(.*)\$\{END_IF.*?\}/gm
-  const regexIfStart = /\$\{IF ((.)*?)\}/gm
-  const regexIfEnd = /\$\{END_IF ((.)*?)\}/gm
+  getPath({ noteContent: noteTemplate ?? '', log, searchObj })
 
   const array = [noteTemplate,
     peopleTemplate,
@@ -104,30 +121,7 @@ export const doIntegration = ({ markdown, pathToTemplates, log, context }: { mar
   ]
 
   const ifArray = array.map(noteContent => {
-    const matches = noteContent?.match(regexIf)
-    console.log('ifArray', { matches })
-
-    matches?.forEach(value => {
-      const ifValue = value.match(regexIfStart)?.[0]
-      if (!ifValue) return
-      const key = (ifValue.replace('${IF ', '').replace('}', '')).toLowerCase()
-
-      const keyValue = searchObject({ obj: searchObj, path: key })
-      console.log({ ifValue, key, keyValue })
-
-      if (!keyValue) {
-        noteContent = noteContent?.replace(value, '')
-      } else {
-        let valueReplaced = value
-        const endIfValue = value.match(regexIfEnd)?.[0]
-        if (ifValue) valueReplaced = valueReplaced?.replace(ifValue, '')
-        if (endIfValue) valueReplaced = valueReplaced?.replace(endIfValue, '')
-        valueReplaced = turnKeyIntoContent({ content: valueReplaced, searchObj }) ?? ''
-        noteContent = noteContent?.replace(value, valueReplaced)
-      }
-    })
-
-    return noteContent
+    return handleConditions({ content: noteContent, searchObj })
   })
 
   log?.(JSON.stringify(ifArray).replaceAll('${', '\\\$\\\{').replaceAll('}', '\\\}').replaceAll(')', '\\\)'))
