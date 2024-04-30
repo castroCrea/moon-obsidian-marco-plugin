9372dafd9f28898bceb08d30f8e52d8ea2cc0ab9
Castro Crea <castro.crea@gmail.com>
2024-04-30 14:21:08 +0200

    ⚰️

--- dist/integration copy.d.ts
+++
@@ -1,8 +0,0 @@
-import { type Context } from '@moonjot/moon';
-import { type LOG } from './types';
-export declare const doIntegration: ({ markdown, pathToTemplate, log, context }: {
-    markdown: string;
-    pathToTemplate?: string | undefined;
-    log: LOG;
-    context: Context;
-}) => boolean;
